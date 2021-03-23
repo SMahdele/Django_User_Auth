9ebc03ea87ea2861cf417972f9335912f1787081
tgyus <madan.lodhi02@gmail.com>
2021-03-23 11:31:35 +0530

    new app user

--- user/views.py
+++ user/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics, status
-from .serializers import UserProjectSerializer, UserEducationSerializer,UserExperienceSerializer
+from .serializers import UserProjectSerializer, UserEducationSerializer, UserExperienceSerializer
 from rest_framework.response import Response
-from authentication.models import  User
-from .models import UserEducation, UserProject,UserExperience
+from authentication.models import User
+from .models import UserEducation, UserProject, UserExperience
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
@@ -13,57 +13,72 @@ from django.core.exceptions import *
 # Create your views here.
 
 class UserProjectView(generics.GenericAPIView):
-    serializer_class=UserProjectSerializer
+    serializer_class = UserProjectSerializer
 
-    def post(self,request,*args,**kwargs):
-            projects=self.request.data
-            serializer = self.serializer_class(data=projects)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            #_data=serializer.data
-            return Response({'data': serializer.data})
+    def post(self, request, *args, **kwargs):
+        projects = self.request.data
+        serializer = self.serializer_class(data=projects)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        # _data=serializer.data
+        return Response({'status': True,
+                         'message': 'project details added successfully',
+                         'data': serializer.data})
+
 
 class UserProjectDetails(APIView):
+    serializer_class = UserProjectSerializer
 
-    serializer_class=UserProjectSerializer
-    def get(self,request,pk):
-            projects=UserProject.objects.filter(user=pk)
-            serializer = UserProjectSerializer(projects, many=True)
-            return Response({
-                        'data':serializer.data
-                   })
-    def put(self,request,pk):
-        id= request.data['id']
+    def get(self, request, pk):
+        projects = UserProject.objects.filter(user=pk)
+        serializer = UserProjectSerializer(projects, many=True)
+        return Response({
+            'status': True,
+            'message': "user project details",
+            'data': serializer.data
+        })
+
+    def put(self, request, pk):
+        id = request.data['id']
         project = UserProject.objects.get(id=id)
-        serializer = self.serializer_class(instance=project,data=self.request.data)
+        serializer = self.serializer_class(instance=project, data=self.request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'data': serializer.data})
+            return Response({'status': True,
+                             'message': " project updated successfully",
+                             'data': serializer.data})
 
-    def delete(self,rquest,pk):
+    def delete(self, rquest, pk):
         project = UserProject.objects.filter(user=pk)
         project.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "user details does not exist"})
 
 
 class UserExperienceView(generics.GenericAPIView):
-    serializer_class=UserExperienceSerializer
+    serializer_class = UserExperienceSerializer
 
-    def post(self,request,*args,**kwargs):
-            projects=self.request.data
-            serializer = self.serializer_class(data=projects)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            #_data=serializer.data
-            return Response({'data': serializer.data})
+    def post(self, request, *args, **kwargs):
+        projects = self.request.data
+        serializer = self.serializer_class(data=projects)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        # _data=serializer.data
+        return Response({'status': True,
+                         'message': 'experience details added successfully',
+                         'data': serializer.data})
+
+
 class UserExperienceDetails(APIView):
-    serializer_class=UserExperienceSerializer
+    serializer_class = UserExperienceSerializer
 
-    def get(self,request,pk):
-            experiences=UserExperience.objects.filter(user=pk)
-            serializer = serializer_class(experiences, many=True)
-            return Response({
-                        'data':serializer.data})
+    def get(self, request, pk):
+        experiences = UserExperience.objects.filter(user=pk)
+        serializer = serializer_class(experiences, many=True)
+        return Response({
+            'status': True,
+            'message': "user experience details ",
+            'data': serializer.data
+        })
 
     def put(self, request, pk):
         id = request.data['id']
@@ -71,42 +86,53 @@ class UserExperienceDetails(APIView):
         serializer = self.serializer_class(instance=experience, data=self.request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'data': serializer.data})
+            return Response({'status': True,
+                             'message': " experience updated successfully",
+                             'data': serializer.data})
 
     def delete(self, rquest, pk):
         experience = UserExperience.objects.filter(user=pk)
         experience.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "user details does not exist"})
 
 
 class UserEducationView(generics.GenericAPIView):
-    serializer_class=UserEducationSerializer
+    serializer_class = UserEducationSerializer
 
-    def post(self,request,*args,**kwargs):
-            education=self.request.data
-            serializer = self.serializer_class(data=education)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            #_data=serializer.data
-            return Response({'data': serializer.data})
+    def post(self, request, *args, **kwargs):
+        education = self.request.data
+        serializer = self.serializer_class(data=education)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        # _data=serializer.data
+        return Response({'status': True,
+                         'message': 'education details added successfully',
+                         'data': serializer.data})
+
+
 class UserEducationDetails(APIView):
+    serializer_class = UserEducationSerializer
 
-    serializer_class=UserEducationSerializer
-    def get(self,request,pk):
-            education=UserEducation.objects.filter(user=pk)
-            serializer = UserEducationSerializer(education, many=True)
-            return Response({
-                        'data':serializer.data
-                   })
-    def put(self,request,pk):
-        id= request.data['id']
+    def get(self, request, pk):
+        education = UserEducation.objects.filter(user=pk)
+        serializer = UserEducationSerializer(education, many=True)
+        return Response({
+            'status': True,
+            'message': "user education details",
+            'data': serializer.data
+        })
+
+    def put(self, request, pk):
+        id = request.data['id']
         education = UserEducation.objects.get(id=id)
-        serializer = self.serializer_class(instance=education,data=self.request.data)
+        serializer = self.serializer_class(instance=education, data=self.request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'data': serializer.data})
+            return Response({'status': True,
+                             'message': " education details updated successfully",
+                             'data': serializer.data})
 
     def delete(self, rquest, pk):
         education = UserEducation.objects.filter(user=pk)
         education.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "user details does not exist"})
